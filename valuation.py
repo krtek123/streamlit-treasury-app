6f7be3f1f3bdcb5ca363a6f26eb6e98bc9231dc4
krtek123 <m.folprecht@centrum.cz>
2024-11-24 19:05:11 +0100

    fix maybe

--- valuation.py
+++ valuation.py
@@ -16,8 +16,8 @@ class FixedBond:
         self.yield_curve_df = yield_curves_df
         self.trade_date = pd.to_datetime(trade_date)
         self.currency_code = currency_code
-        self.principal_payment_frequency = principal_payment_frequency
-        self.coupon_frequency = coupon_frequency
+        self.principal_payment_frequency = bond_data["Principal Payment Frequency"]
+        self.coupon_frequency = bond_data["Coupon Frequency"]
         self.business_day_convention = bond_data["Business Day Convention"]
         self.day_count_convention = bond_data["Day Count Convention"]  # Added Day Count Convention
         self.number_of_pieces = number_of_pieces
